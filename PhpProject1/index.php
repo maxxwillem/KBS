641d305e6ef2d63b122f161e48ebf0c403374554
unknown <rick_1996@hotmail.com>
2014-11-12 10:28:26 +0100

    Initial commit

---
+++ PhpProject1/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+Naam: Danny Zwiggelaar
+Studentnummer: 1079717
+Klas: ICTm1F
+Collegejaar: 2014-2015
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "Hallo Allemaal";
+        ?>
+    </body>
+</html>
---
+++ README.md
@@ -0,0 +1,4 @@
+STC01
+=====
+
+Het STC01 KBS project met dank aan dzmen
