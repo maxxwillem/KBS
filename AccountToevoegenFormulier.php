de6290b4614d9bb89a4cdaddc1dfc318f3ca59bf
michiel_mak <michiel_mak@85-203.dhcp.windesheim.nl>
2014-11-19 13:40:11 +0100

    AccountToevoegenFormulier--Bootstrap gedaan!!

--- AccountToevoegenFormulier.php
+++ AccountToevoegenFormulier.php
@@ -48,118 +48,135 @@
                 <div class="col-md-9">
                     <div class="panel panel-default">
                         <div class="panel-body">
-                            <form method="POST" action="AccountToevoegenFormulier.php">
-                                <table>
-                                    <tr class="header"><td colspan="2"><h1>Toevoegen Account</h1></td></tr>
+                            <form class="form-horizontal" role="form" method="POST">
 
 
-                                    <tr>
-                                        <td>Gebruikersnaam:</td><td><input type="text" name="Gebruikersnaam" value="<?= $_POST['Gebruikersnaam'] ?>"></td>
 
-                                        <?php
-                                        if (isset($_POST['Toevoegen'])) {
-                                            if (empty($_POST['Gebruikersnaam'])) {
-                                                print("<td>");
-                                                print("verplicht!");
-                                                print("</td>");
-                                            }
-                                        }
-                                        ?>
-                                    </tr>
-                                    <tr>
-                                        <td> Naam :</td><td><input type="text" name="Naam" value="<?= $_POST['Naam'] ?>"></td>
-                                        <?php
-                                        if (isset($_POST['Toevoegen'])) {
-                                            if (empty($_POST['Naam'])) {
-                                                print("<td>");
-                                                print("verplicht!");
-                                                print("</td>");
-                                            }
-                                        }
-                                        ?>
-                                    </tr>
-                                    <tr>
-                                        <td>Achternaam :</td><td><input type="text" name="Achternaam" value="<?= $_POST['Achternaam'] ?>"></td>
-                                        <?php
-                                        if (isset($_POST['Toevoegen'])) {
-                                            if (empty($_POST['Achternaam'])) {
-                                                print("<td>");
-                                                print("verplicht!");
-                                                print("</td>");
-                                            }
-                                        }
-                                        ?>
-                                    </tr>
-                                    <tr><td> Functie :</td><td>
-                                            <SELECT NAME="Functie">
-                                                <OPTION VALUE="Empty">-----</OPTION>
-                                                <OPTION VALUE="Beheerder">Beheerder</OPTION>
-                                                <OPTION VALUE="Senior">Senior</OPTION>
-                                                <OPTION VALUE="Technici">Technici</OPTION>
-                                            </SELECT>  
-                                        </td>
-                                        <?php
-                                        if (isset($_POST['Toevoegen'])) {
-                                            if (($_POST['Functie'] == "Empty")) {
-                                                print("<td>");
-                                                print("verplicht!");
-                                                print("</td>");
-                                            }
-                                        }
-                                        ?>
-                                    </tr>
-                                    <tr>
-                                        <td>Wachtwoord :</td><td><input type="password" name="password"></td>
-                                        <?php
-                                        // kijk of het wachtwoord voldoet aan de criteria, en print error message indien nodig
-                                        if (isset($_POST['Toevoegen'])) {
-                                            $password_error = array();
-                                            if (strlen($wachtwoord) < 8) {
-                                                $password_error[] = 'Minimaal 8 karakters lang.';
-                                            }
-                                            if (!preg_match('/[A-Z]/', $wachtwoord)) {
-                                                $password_error[] = 'Minimaal 1 hoofdletter.';
-                                            }
-                                            if (!preg_match('/[0-9]/', $wachtwoord)) {
-                                                $password_error[] = 'Minimaal 1 getal';
-                                            }
-                                            if ($wachtwoord != $wachtwoord_herhaal) {
-                                                $password_error[] = 'Wachtwoorden zijn niet gelijk';
-                                            }
-                                            $password_error_count = count($password_error);
-                                            if ($password_error_count > 0) {
-                                                print("<td>");
-                                                for ($i = 0; $i < $password_error_count; $i++) {
 
-                                                    print( $password_error[$i] . "<br>");
-                                                }
-                                                print("</td>");
-                                            }
-                                        }
-                                        ?>
-                                    </tr>
-                                    <tr>
-                                        <td>Wachtwoord bevestigen :</td><td><input type="password" name="passwordcontrole"></td>
-                                    </tr>
-                                    <tr>
-                                        <td colspan="2"><input type="submit" name="Annuleren" value="Annuleren">
-                                            <input type="submit" name="Toevoegen" value="Toevoegen"></td>
+                                <div class="form-group">
 
-                                    </tr>   
-                                </table>
+                                    <label class="col-sm-2 control-label">Gebruikersnaam:</label>
+                                    <div class="col-sm-3">
+                                        <input class="form-control" type="text" name="Gebruikersnaam" value="<?= $_POST['Gebruikersnaam'] ?>">
+                                 
+                                <?php
+                                if (isset($_POST['Toevoegen'])) {
+                                    if (empty($_POST['Gebruikersnaam'])) {
+                                        
+                                        print("verplicht!");
+                                        
+                                    }
+                                }
+                                ?>
+                                           </div>
+                                </div>
+                                <div class="form-group">   
+                                    <label class="col-sm-2 control-label">Naam:</label>
+                                    <div class="col-sm-3">
+                                        <input class="form-control" type="text" name="Naam" value="<?= $_POST['Naam'] ?>">
+                                    
+                                <?php
+                                if (isset($_POST['Toevoegen'])) {
+                                    if (empty($_POST['Naam'])) {
+                                        
+                                        print("verplicht!");
+                                        
+                                    }
+                                }
+                                ?>
+                                        </div></div>
+                                <div class="form-group">   
+                                    <label class="col-sm-2 control-label">Achternaam:</label>
+                                    <div class="col-sm-3">                             
+                                        <input class="form-control" type="text" name="Achternaam" value="<?= $_POST['Achternaam'] ?>">
+
+                                    
+                                <?php
+                                if (isset($_POST['Toevoegen'])) {
+                                    if (empty($_POST['Achternaam'])) {
+                                        print("verplicht!");
+                                    }
+                                }
+                                ?>
+                                        </div></div>
+                                <div class="form-group">   
+                                    <label class="col-sm-2 control-label">Functie:</label>
+                                    <div class="col-sm-3">
+                                        <SELECT class="form-control" NAME="Functie">
+                                            <OPTION VALUE="Empty">-----</OPTION>
+                                            <OPTION VALUE="Beheerder">Beheerder</OPTION>
+                                            <OPTION VALUE="Senior">Senior</OPTION>
+                                            <OPTION VALUE="Technici">Technici</OPTION>
+                                        </SELECT>  
+                                    
+                                <?php
+                                if (isset($_POST['Toevoegen'])) {
+                                    if (($_POST['Functie'] == "Empty")) {
+                                        print("verplicht!");
+                                    }
+                                }
+                                ?>
+                                                                                </div></div>
+
+                                <div class="form-group">   
+                                    <label class="col-sm-2 control-label">Wachtwoord:</label>
+                                    <div class="col-sm-3"> 
+                                        <input type="password" name="password">
+                                <?php
+                                // kijk of het wachtwoord voldoet aan de criteria, en print error message indien nodig
+                                if (isset($_POST['Toevoegen'])) {
+                                    $password_error = array();
+                                    if (strlen($wachtwoord) < 8) {
+                                        $password_error[] = 'Minimaal 8 karakters lang.';
+                                    }
+                                    if (!preg_match('/[A-Z]/', $wachtwoord)) {
+                                        $password_error[] = 'Minimaal 1 hoofdletter.';
+                                    }
+                                    if (!preg_match('/[0-9]/', $wachtwoord)) {
+                                        $password_error[] = 'Minimaal 1 getal';
+                                    }
+                                    if ($wachtwoord != $wachtwoord_herhaal) {
+                                        $password_error[] = 'Wachtwoorden zijn niet gelijk';
+                                    }
+                                    $password_error_count = count($password_error);
+                                    if ($password_error_count > 0) {
+                                        print("<td>");
+                                        for ($i = 0; $i < $password_error_count; $i++) {
+
+                                            print( $password_error[$i] . "<br>");
+                                        }
+                                        print("</td>");
+                                    }
+                                }
+                                ?>
+                                                                            </div></div>
+
+                                <div class="form-group">   
+                                    <label class="col-sm-2 control-label">Wachtwoord Bevestigen:</label>
+                                    <div class="col-sm-3"> 
+                                        <input type="password" name="passwordcontrole">
+                                    </div></div>
+                                <div class="form-group">   
+
+                                    <div class="col-sm-offset-2"> 
+                                        <input type="submit" name="Annuleren" value="Annuleren">
+                                        <input type="submit" name="Toevoegen" value="Toevoegen">
+
+                                    </div></div>
+
                             </form>  
                             <?php
                             if (isset($_POST['Toevoegen'])) {
-									$gebruikersnaam = $_POST['Gebruikersnaam'];
-									$naam = $_POST['Naam'];
-									$achternaam = $_POST['Achternaam'];
-									$functie = $_POST['Functie'];
-									$wachtwoord = $_POST['password'];
-									$wachtwoord_herhaal = $_POST['passwordcontrole'];
+                                $gebruikersnaam = $_POST['Gebruikersnaam'];
+                                $naam = $_POST['Naam'];
+                                $achternaam = $_POST['Achternaam'];
+                                $functie = $_POST['Functie'];
+                                $wachtwoord = $_POST['password'];
+                                $wachtwoord_herhaal = $_POST['passwordcontrole'];
                                 if (!empty($gebruikersnaam) && !empty($naam) && !empty($achternaam) && $functie != "Empty" && !empty($wachtwoord)) {
-									
-									require "Database/mysql_connect.php";
-									
+
+                                    require "Database/mysql_connect.php";
+
                                     //haal password op en maak een "salt" aan<---encrypt het wachtwoord
                                     $wachtwoord = htmlspecialchars($wachtwoord);
                                     $salt = [
