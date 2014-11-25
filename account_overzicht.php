998244b6f630c19fec6a0f6fc0869b714758fdbb
CodedWrench <rick_1996@hotmail.com>
2014-11-25 10:13:01 +0100

    Wijzigingen en toevoegen wachtwoord wijzigen

--- account_overzicht.php
+++ account_overzicht.php
@@ -50,7 +50,7 @@
                         <a href="#" class="list-group-item">Basestation onderhoud</a>
                         <a href="account_overzicht.php" class="list-group-item active">Account beheer</a>
                         <a href="basestation_overzicht.php" class="list-group-item">Basestation Overzicht</a>
-                        <a href="#" class="list-group-item">Wachtwoord wijzigen</a>
+                        <a href="wijzigen_wachtwoord.php" class="list-group-item">Wachtwoord wijzigen</a>
                         <a href="Logout.php" class="list-group-item">Uitloggen</a>
                         <div class="list-group-item empty"></div>
                     </div>
